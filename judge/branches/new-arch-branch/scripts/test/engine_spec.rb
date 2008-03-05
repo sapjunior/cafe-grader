f281dbc4c686e20cdb06b5bf43c186bb43c950cf
jittat <jittat@750a85d1-f453-0410-8525-03323006c405>
2008-03-05 17:58:25 +0000

    moved helper out of engine_spec

--- judge/branches/new-arch-branch/scripts/test/engine_spec.rb
+++ judge/branches/new-arch-branch/scripts/test/engine_spec.rb
@@ -1,38 +1,5 @@
-#require 'test/unit'
-#require 'rubygems'
-#require 'mocha'
-
 require File.join(File.dirname(__FILE__),'spec_helper')
-
-module GraderEngineHelperMethods
-
-  def clear_sandbox
-    config = Grader::Configuration.get_instance
-    clear_cmd = "rm -rf #{config.test_sandbox_dir}/*"
-    system(clear_cmd)
-  end
-
-  def init_sandbox
-    config = Grader::Configuration.get_instance
-    clear_sandbox
-    Dir.mkdir config.user_result_dir
-    cp_cmd = "cp -R #{config.test_data_dir}/ev #{config.test_sandbox_dir}"
-    system(cp_cmd)
-  end
-
-  def create_submission_from_file(id, user, problem, 
-                                  source_fname, language=nil)
-
-    language = stub(Language, :name => 'c', :ext => 'c') if language==nil
-
-    config = Grader::Configuration.get_instance
-    source = File.open(config.test_data_dir + "/" + source_fname).read
-    stub(Submission,
-         :id => id, :user => user, :problem => problem,
-         :source => source, :language => language)
-  end
-
-end
+require File.join(File.dirname(__FILE__),'engine_spec_helper')
 
 describe "A grader engine, when grading a submission" do
 
