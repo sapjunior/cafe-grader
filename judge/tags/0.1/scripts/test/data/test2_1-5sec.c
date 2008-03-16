3c4c5c751f255a4d3a0a49da67a77c963d22b9fd
pramook <pramook@750a85d1-f453-0410-8525-03323006c405>
2008-03-16 12:00:14 +0000

    judge: Create version 0.1 for HEAD of the trunk.

---
+++ judge/tags/0.1/scripts/test/data/test2_1-5sec.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+  int a,b;
+
+  int c=0;
+
+  scanf("%d %d",&a,&b);
+  printf("%d\n",a+b);
+
+  for(a=0; a<2; a++) {
+    while(c<1150000000) {
+      c++;
+      b+=c;
+    }
+  }
+  exit(0);
+}
+
