f1d5b6c21f8dbba7414ccefb4dfbe873347e627d
jittat <jittat@750a85d1-f453-0410-8525-03323006c405>
2009-02-02 10:27:23 +0000

    added option for early test_request timeout

--- web/branches/ytopc08-2/db/schema.rb
+++ web/branches/ytopc08-2/db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20081215094303) do
+ActiveRecord::Schema.define(:version => 20090202101806) do
 
   create_table "announcements", :force => true do |t|
     t.string   "author"
