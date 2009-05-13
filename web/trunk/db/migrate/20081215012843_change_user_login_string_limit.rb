8fdaf04f699a00f02774e994e5a420211dd5d6dc
jittat <jittat@750a85d1-f453-0410-8525-03323006c405>
2009-05-13 04:44:54 +0000

    removed broken trunk

--- web/trunk/db/migrate/20081215012843_change_user_login_string_limit.rb
+++
@@ -1,9 +0,0 @@
-class ChangeUserLoginStringLimit < ActiveRecord::Migration
-  def self.up
-    execute "ALTER TABLE `users` CHANGE `login` `login` VARCHAR( 50 )"
-  end
-
-  def self.down
-    execute "ALTER TABLE `users` CHANGE `login` `login` VARCHAR( 10 )"
-  end
-end
